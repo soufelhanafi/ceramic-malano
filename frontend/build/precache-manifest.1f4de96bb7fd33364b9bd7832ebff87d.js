e785f2e12148da35ca643c0dbe5025adb563a449
Soufian Elhanafi <soufelhanafi@gmail.com>
2020-09-03 11:05:11 +0100

    update gitigbite

---
+++ frontend/build/precache-manifest.1f4de96bb7fd33364b9bd7832ebff87d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75ed5b63f7dd696957ed9107460c581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f54e82e0acc3f68442",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "ac361491d2284e04ab74",
+    "url": "/static/css/main.544923e8.chunk.css"
+  },
+  {
+    "revision": "60f54e82e0acc3f68442",
+    "url": "/static/js/2.51b55015.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "/static/js/2.51b55015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac361491d2284e04ab74",
+    "url": "/static/js/main.9ad2fe11.chunk.js"
+  },
+  {
+    "revision": "00218a4a11e9c2bb1b3f",
+    "url": "/static/js/runtime-main.e2ec618b.js"
+  },
+  {
+    "revision": "568701e89cd7f15c51f081b70410c50e",
+    "url": "/static/media/logo1.568701e8.jpeg"
+  }
+]);
